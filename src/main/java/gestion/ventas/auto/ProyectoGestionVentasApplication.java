1c9029f38f73d8707126fc453cc2a62226d22406
deanAnder <dean2anderhm@gmail.com>
2024-10-14 09:06:14 -0500

    Publicando Proyecto

---
+++ src/main/java/gestion/ventas/auto/ProyectoGestionVentasApplication.java
@@ -0,0 +1,13 @@
+package gestion.ventas.auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGestionVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGestionVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestion/ventas/auto/ProyectoGestionVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package gestion.ventas.auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGestionVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
