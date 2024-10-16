0ece5db8c4a980b2abd53c4feb452c551c9a40a1
BrianAnton <briancondori530@gmail.com>
2024-10-16 10:51:26 -0500

    models

--- src/main/java/gestion/ventas/auto/models/Usuario.java
+++ src/main/java/gestion/ventas/auto/models/Usuario.java
@@ -1,4 +1,40 @@
 package gestion.ventas.auto.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private int idUsuario;
+    
+    private String nombre;
+    
+    private String apellido;
+    
+    @Column(length = 9)
+    private String celular;
+    
+    @Column(name = "nombreUsuario", length = 4, nullable = false)
+    private String nombreUsuario;
+    
+    @Column(name = "contraseña", length = 4, nullable = false)
+    private String contraseña;
 }
+
+	
+	
+
+
+
--- src/main/java/gestion/ventas/auto/models/Ventas.java
+++ src/main/java/gestion/ventas/auto/models/Ventas.java
@@ -1,13 +1,39 @@
 package gestion.ventas.auto.models;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ventas {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_ventas")
     private Integer idVenta;
+    
+    @Column(name = "numero_venta", length = 4)
+    private String numeroVenta;
+
+    @Column(name = "fecha_venta", nullable = false)
+    private LocalDateTime fechaVenta;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente")
+    private Clientes cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario")
+    private Usuario usuario;
 }
