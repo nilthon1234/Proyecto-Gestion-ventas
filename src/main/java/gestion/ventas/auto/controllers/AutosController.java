03236c5def977cf1da7111e9f1ef00ca680d3bcd
deanAnder <dean2anderhm@gmail.com>
2024-10-17 16:44:11 -0500

    reportes

--- src/main/java/gestion/ventas/auto/controllers/AutosController.java
+++ src/main/java/gestion/ventas/auto/controllers/AutosController.java
@@ -3,21 +3,35 @@ package gestion.ventas.auto.controllers;
 import gestion.ventas.auto.dto.AutoDTO;
 import gestion.ventas.auto.models.Autos;
 import gestion.ventas.auto.service.ServiceAutos;
+import jakarta.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.repo.Resource;
+
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 @RestController
 @RequestMapping("/api/auto")
 public class AutosController {
-
+	
+	private final DataSource dataResource;
+	private final ResourceLoader resourceLoader;
     private final ServiceAutos serviceAutos;
-    public AutosController (ServiceAutos serviceAutos){
-        this.serviceAutos = serviceAutos;
+    public AutosController (ServiceAutos serviceAutos, DataSource dataResource, ResourceLoader resourceLoader){
+        this.dataResource = dataResource;
+		this.resourceLoader = resourceLoader;
+		this.serviceAutos = serviceAutos;
     }
     @GetMapping
     public List<AutoDTO> listarTodo (){
@@ -41,6 +55,19 @@ public class AutosController {
 
         }catch (RuntimeException e){
           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+    @GetMapping("/reporte")
+    public void generarPDFCliente(HttpServletResponse response) {
+        response.setHeader("Content-Disposition", "inline;");
+        response.setContentType("application/pdf");
+        try {
+            String ru = resourceLoader.getResource("classpath:static/report/lista_auto.jasper").getURI().getPath();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(ru,null, dataResource.getConnection());
+            OutputStream outStream = response.getOutputStream();
+            JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
